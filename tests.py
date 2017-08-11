64d71f422f87a5321c1e428afb6aa938332af42c
Ranc58 <rancvova@gmail.com>
2017-08-11 16:54:41 +0300

    change find method to css_selector some small fixes

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import unittest
+from time import sleep # TODO del sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,56 +14,63 @@ TIMEOUT = 10
 
 class AtomicPrj(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.PhantomJS()
+        self.driver = webdriver.PhantomJS('D:\phantomjs-2.1.1-windows\\bin\phantomjs.exe')
+        #self.driver = webdriver.Chrome('D:\chromedriver.exe')
         driver = self.driver
         driver.get(CREATE_USER_URL)
         self.assertIn("Создать тестового пользователя", driver.title)
         driver.find_element_by_xpath("/html/body/form/button").click()
         driver.get('http://atomicboard.devman.org/#/')
-        load_tasks = driver.find_element_by_css_selector("a[href='/']")
+        load_tasks = driver.find_element_by_css_selector("a[href='/']") # todo change to CSS
         load_tasks.click()
         wait = WebDriverWait(driver, TIMEOUT)
-        self.tasks = '//span[@class="col-md-10 js-board-wrapper"]'
+        self.tasks = 'span.col-md-10.js-board-wrapper'
         wait.until(EC.visibility_of_element_located
-                   ((By.XPATH, self.tasks)))
+                   ((By.CSS_SELECTOR, self.tasks)))
 
+    @unittest.skip("demonstrating skipping")
     def test_view_tasks(self):
-        self.assertTrue(self.driver.find_element_by_xpath
-                        (self.tasks))
+        self.assertTrue(self.driver.find_element_by_css_selector(
+                        self.tasks))
 
+    @unittest.skip("demonstrating skipping")
     def test_ticket_condition_switch(self):
         driver = self.driver
         wait = WebDriverWait(driver, TIMEOUT)
-        switcher = '//span[@class="badge ticket_status ng-binding"]'
+        switcher = 'span.badge.ticket_status.ng-binding'
         wait.until(EC.visibility_of_element_located
-                   ((By.XPATH, switcher)))
-        switcher_button = driver.find_element_by_xpath(switcher)
+                   ((By.CSS_SELECTOR, switcher)))
+        switcher_button = driver.find_element_by_css_selector(switcher)
         switcher_button.click()
-        switch_menu = '//*[@id="changeStatusModal"]/div/div'
+        switch_menu = 'div.modal-body.js-change-status-form.change-status-form'
         wait.until(EC.visibility_of_element_located
-                   ((By.XPATH, switch_menu)))
-        close_task_button = driver.find_element_by_xpath(
-            '//*[@id="changeStatusModal"]//div[2]/div[3]/button')
+                   ((By.CSS_SELECTOR, switch_menu)))
+        close_task_button = driver.find_element_by_css_selector(
+            'button.btn.btn-lg.btn-primary.change-status-form__button')
         close_task_button.click()
         self.assertEqual('closed', switcher_button.text)
 
+    @unittest.skip("demonstrating skipping")
     def test_edit_task(self):
+        test_edit_task_title = 'Test edit'
         wait = WebDriverWait(self.driver, TIMEOUT)
-        task_text = "//span[1]/div/span[2]/div[2]/div[1]/span[1]"
+        task_text = "span.panel-heading_text.js-panel-heading_text"
         wait.until(EC.
                    element_to_be_clickable
-                   ((By.XPATH, task_text)))
-        task_text_field = self.driver.find_element_by_xpath(task_text)
+                   ((By.CSS_SELECTOR, task_text)))
+        task_text_field = self.driver.find_element_by_css_selector(
+            task_text)
         task_text_field.click()
-        task_text_field_for_edit = self.driver.find_element_by_xpath(
-            '//span[1]/div/span[2]/div[2]/div[1]/form/div/input')
-        task_text_field_for_edit.clear()
-        task_text_field_for_edit.send_keys('Test edit')
-        submit_text_change_button = self.driver.find_element_by_xpath(
-            '//span[2]/div[2]/div[1]/form/div/span/button[1]')
-        submit_text_change_button.click()
-        self.assertEqual('Test edit', task_text_field.text)
+        text_field_for_edit = self.driver.find_element_by_css_selector(
+            'input.editable-has-buttons')
+        text_field_for_edit.clear()
+        text_field_for_edit.send_keys(test_edit_task_title)
+        submit_button = self.driver.find_element_by_css_selector(
+            'span.editable-buttons > button.btn.btn-primary')
+        submit_button.click()
+        self.assertEqual(test_edit_task_title, task_text_field.text)
 
+    @unittest.skip("demonstrating skipping")
     def test_moving_tasks(self):
         with open("jquery_load_helper.js") as f:
             load_jquery_js = f.read()
@@ -69,41 +78,44 @@ class AtomicPrj(unittest.TestCase):
             drag_and_drop_js = f.read()
         wait = WebDriverWait(self.driver, TIMEOUT)
         wait.until(EC.visibility_of_element_located(
-            (By.XPATH, "//div[2]/span[1]/div/span[2]/div[1]/h3")))
+            (By.CSS_SELECTOR,
+             "div.panel-heading-no-padding.ticket__importance-5")))
         srcipt_timeout_secononds = 30
         self.driver.set_script_timeout(srcipt_timeout_secononds)
         self.driver.execute_async_script(load_jquery_js, JQUERY_URL)
         # disregard PEP8 because selenium don't support drag&drop,
-        # need JS
+        # need JS.
+        # https://stackoverflow.com/a/29381532/8106659
         self.driver.execute_script(
             drag_and_drop_js + "$('div.ticket__compact:eq(0)').simulateDragDrop({ dropTarget: 'span.tickets-column:eq(1)'});")
-        moved_ticket = self.driver.find_element_by_xpath(
-            '//span[1]/div/span[2]/div[2]/div[1]/span[1]')
+        moved_ticket = self.driver.find_element_by_css_selector(
+            'span.js-panel-heading_text:nth-child(2)')
         expected = 'Расчет радиационной стойкости. Пнуть математиков'
         self.assertEqual(expected, moved_ticket.text)
 
+
     def test_create_new_task(self):
+        test_task_title = 'New test ticket'
         wait = WebDriverWait(self.driver, TIMEOUT)
-        add_task_field_xpath = "//span[1]/div/span[2]/div[3]/span"
-        wait.until(EC.element_to_be_clickable((
-            By.XPATH, add_task_field_xpath)))
-        add_task_field = self.driver.find_element_by_xpath(
-            add_task_field_xpath)
+        add_task_field_css = "span.add-ticket-block_button"
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, add_task_field_css)))
+        add_task_field = self.driver.find_element_by_css_selector(add_task_field_css)
         add_task_field.click()
-        new_task_text_field_xpath = '//span[2]/div[3]/form/div/input'
-        new_task_text_field = self.driver.find_element_by_xpath(
-            new_task_text_field_xpath)
-        new_task_text_field.send_keys('New test ticket')
-        submit_new_task = '//span[2]/div[3]/form/div/span/button[1]'
-        submit_new_task_button = self.driver.find_element_by_xpath(
+        new_task_text_field_css = 'input.editable-has-buttons'
+        new_task_text_field = self.driver.find_element_by_css_selector(
+            new_task_text_field_css)
+        new_task_text_field.send_keys(test_task_title)
+        submit_new_task = 'button.btn.btn-primary'
+        submit_task_button = self.driver.find_element_by_css_selector(
             submit_new_task)
-        submit_new_task_button.click()
-        added_task_xpath = '//div/span[2]/div[3]/div[1]/span[1]'
-        wait.until(EC.visibility_of_element_located((
-            By.XPATH, added_task_xpath)))
-        added_task = self.driver.find_element_by_xpath(
-            added_task_xpath)
-        self.assertEqual('New test ticket', added_task.text)
+        submit_task_button.click()
+        task_css = 'span.panel-heading_text'
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span.panel-heading_text:contains[innertext="New test ticket"]')))
+        tasks = self.driver.find_elements_by_css_selector(
+            task_css)
+        #added_task_number = 1
+        self.assertEqual(test_task_title,
+                         tasks[1].text)
 
     def tearDown(self):
         self.driver.close()
