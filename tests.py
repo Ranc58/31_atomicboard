3957b58f15dae9b8c8f6bb965f28526f9e676074
rancvova@gmail.com <Unseen82>
2017-08-13 13:41:31 +0300

    Add xpath for find added task element. del unuseable chrome webdriver. some small fixes.

--- tests.py
+++ tests.py
@@ -1,11 +1,9 @@
 import unittest
-from time import sleep # TODO del sleep
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 CREATE_USER_URL = "http://atomicboard.devman.org/create_test_user/"
 JQUERY_URL = "http://code.jquery.com/jquery-1.11.2.min.js"
@@ -14,26 +12,24 @@ TIMEOUT = 10
 
 class AtomicPrj(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.PhantomJS('D:\phantomjs-2.1.1-windows\\bin\phantomjs.exe')
-        #self.driver = webdriver.Chrome('D:\chromedriver.exe')
+        self.driver = webdriver.PhantomJS()
         driver = self.driver
+        driver.implicitly_wait(10)
         driver.get(CREATE_USER_URL)
         self.assertIn("Создать тестового пользователя", driver.title)
         driver.find_element_by_xpath("/html/body/form/button").click()
         driver.get('http://atomicboard.devman.org/#/')
-        load_tasks = driver.find_element_by_css_selector("a[href='/']") # todo change to CSS
+        load_tasks = driver.find_element_by_css_selector("a[href='/']")
         load_tasks.click()
         wait = WebDriverWait(driver, TIMEOUT)
         self.tasks = 'span.col-md-10.js-board-wrapper'
         wait.until(EC.visibility_of_element_located
                    ((By.CSS_SELECTOR, self.tasks)))
 
-    @unittest.skip("demonstrating skipping")
     def test_view_tasks(self):
         self.assertTrue(self.driver.find_element_by_css_selector(
                         self.tasks))
 
-    @unittest.skip("demonstrating skipping")
     def test_ticket_condition_switch(self):
         driver = self.driver
         wait = WebDriverWait(driver, TIMEOUT)
@@ -42,15 +38,14 @@ class AtomicPrj(unittest.TestCase):
                    ((By.CSS_SELECTOR, switcher)))
         switcher_button = driver.find_element_by_css_selector(switcher)
         switcher_button.click()
-        switch_menu = 'div.modal-body.js-change-status-form.change-status-form'
-        wait.until(EC.visibility_of_element_located
-                   ((By.CSS_SELECTOR, switch_menu)))
+        switch_menu = 'div.js-change-status-form.change-status-form'
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,
+                                                     switch_menu)))
         close_task_button = driver.find_element_by_css_selector(
             'button.btn.btn-lg.btn-primary.change-status-form__button')
         close_task_button.click()
         self.assertEqual('closed', switcher_button.text)
 
-    @unittest.skip("demonstrating skipping")
     def test_edit_task(self):
         test_edit_task_title = 'Test edit'
         wait = WebDriverWait(self.driver, TIMEOUT)
@@ -70,7 +65,6 @@ class AtomicPrj(unittest.TestCase):
         submit_button.click()
         self.assertEqual(test_edit_task_title, task_text_field.text)
 
-    @unittest.skip("demonstrating skipping")
     def test_moving_tasks(self):
         with open("jquery_load_helper.js") as f:
             load_jquery_js = f.read()
@@ -82,40 +76,42 @@ class AtomicPrj(unittest.TestCase):
              "div.panel-heading-no-padding.ticket__importance-5")))
         srcipt_timeout_secononds = 30
         self.driver.set_script_timeout(srcipt_timeout_secononds)
-        self.driver.execute_async_script(load_jquery_js, JQUERY_URL)
+        self.driver.execute_async_script(load_jquery_js,
+                                         JQUERY_URL)
         # disregard PEP8 because selenium don't support drag&drop,
         # need JS.
         # https://stackoverflow.com/a/29381532/8106659
         self.driver.execute_script(
-            drag_and_drop_js + "$('div.ticket__compact:eq(0)').simulateDragDrop({ dropTarget: 'span.tickets-column:eq(1)'});")
+            drag_and_drop_js +
+            "$('div.ticket__compact:eq(0)').simulateDragDrop({ dropTarget: 'span.tickets-column:eq(1)'});")
         moved_ticket = self.driver.find_element_by_css_selector(
             'span.js-panel-heading_text:nth-child(2)')
         expected = 'Расчет радиационной стойкости. Пнуть математиков'
         self.assertEqual(expected, moved_ticket.text)
 
-
     def test_create_new_task(self):
-        test_task_title = 'New test ticket'
+        test_task_title = 'New test task'
         wait = WebDriverWait(self.driver, TIMEOUT)
         add_task_field_css = "span.add-ticket-block_button"
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, add_task_field_css)))
-        add_task_field = self.driver.find_element_by_css_selector(add_task_field_css)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                               add_task_field_css)))
+        add_task_field = self.driver.find_element_by_css_selector(
+            add_task_field_css)
         add_task_field.click()
         new_task_text_field_css = 'input.editable-has-buttons'
         new_task_text_field = self.driver.find_element_by_css_selector(
             new_task_text_field_css)
         new_task_text_field.send_keys(test_task_title)
-        submit_new_task = 'button.btn.btn-primary'
-        submit_task_button = self.driver.find_element_by_css_selector(
-            submit_new_task)
-        submit_task_button.click()
-        task_css = 'span.panel-heading_text'
-        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span.panel-heading_text:contains[innertext="New test ticket"]')))
-        tasks = self.driver.find_elements_by_css_selector(
-            task_css)
-        #added_task_number = 1
-        self.assertEqual(test_task_title,
-                         tasks[1].text)
+        new_task_text_field.send_keys(Keys.ENTER)
+        mounth_tasks = self.driver.find_element_by_css_selector(
+            'span.col-md-4.tickets-column')
+        new_task_xpath = '//span[contains(.,"New test task")]'
+        wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                     new_task_xpath)))
+        added_task_index = -1
+        new_task = mounth_tasks.find_elements_by_css_selector(
+            'span.panel-heading_text')[added_task_index]
+        self.assertEqual(test_task_title, new_task.text)
 
     def tearDown(self):
         self.driver.close()
